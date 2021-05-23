fd687bb43981a15294472033374a972216a40cc7
Dmitrii Rodygin <DRodygin2@slb.com>
2021-05-23 15:37:06 +0300

    Lesson_2_task_1

--- Lesson_1_task_2.js
+++
@@ -1,9 +0,0 @@
-// Объявить две переменные: admin и name. Записать в name строку "Василий"; 
-// Скопировать значение из name в admin. Вывести admin (должно вывестись «Василий»).
-
-"use strict"
-
-var admin;
-var name = "Василий";
-admin = name;
-console.log(admin);
